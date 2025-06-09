e6260840e6d4967c283273952b7649f2510e2c70
Parisj14180 <paris.jones14180@gmail.com>
2025-06-09 18:14:08 -0400

    leafnode and test added

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,7 @@
+from textnode import TextNode
+from textnode import TextType
+
+
 class HTMLNode:
     def __init__(self=None, tag=None, value=None, children=None, props=None):
         self.tag = tag
@@ -8,11 +12,58 @@ class HTMLNode:
     def to_html(self):
         raise NotImplementedError
     def props_to_html(self):
+        if self.props is None:
+            return ""
         result = ""
         for key, value in self.props.items():
             result += f' {key}="{value}"'
-
         return result
     
     def __repr__(self):
         return f"HTMLNode(tag='{self.tag}', value='{self.value}', children={self.children}, props={self.props})"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+           raise ValueError("All leaf nodes must have a value")
+        if self.tag is None:
+           return self.value
+        else:
+            props_str = self.props_to_html()
+            return f'<{self.tag}{props_str}>{self.value}</{self.tag}>'
+        
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("no tag found")
+        if self.children is None or self.children == []:
+            raise ValueError("children missing")
+        
+        result = ""
+        for child in self.children:
+            result += child.to_html()
+        return f"<{self.tag}>{result}</{self.tag}>"
+    
+def text_node_to_html_node(text_node):
+    if text_node.text_type != TextType.TEXT and text_node.text_type != TextType.BOLD and text_node.text_type != TextType.ITALIC and text_node.text_type != TextType.CODE and text_node.text_type != TextType.LINK and text_node.text_type != TextType.IMAGE:
+        raise Exception("invalid")
+    elif text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode("b")
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode("i")
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode("code")
+    elif text_node.text_type == TextType.LINK:
+        return LeafNode("a[href]")
+    elif text_node.text_type == TextType.IMAGE:
+        return LeafNode("img","", "src", "alt")
+    
---
+++ src/test_leafnode.py
@@ -0,0 +1,23 @@
+import unittest
+from htmlnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_b(self):
+        node = LeafNode("b", "Hello, world!")
+        self.assertEqual(node.to_html(), "<b>Hello, world!</b>")
+
+    def test_leaf_to_html_a_with_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+    def test_leaf_to_html_no_value_raises_error(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+if __name__ == "__main__":
+    unittest.main()
