11003e11541a4f4293058ea9d4100db93aef7f45
Parisj14180 <paris.jones14180@gmail.com>
2025-06-11 11:20:25 -0400

    more tests implemented

---
+++ src/test_text.py
@@ -0,0 +1,25 @@
+import unittest
+from htmlnode import text_node_to_html_node
+from textnode import TextNode, TextType
+
+class TestTextNodeToHTML(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_text_to_bold(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold text node")
+
+    def test_text_to_italic(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is a bold text node")
+
+if __name__ == "__main__":
+    unittest.main()
