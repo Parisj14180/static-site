a3a70a4ebdd2b4b26e907290daa986aed0cde4ee
Parisj14180 <paris.jones14180@gmail.com>
2025-06-14 11:18:46 -0400

    markdown extractors and test added

--- src/node_delimiter.py
+++ src/node_delimiter.py
@@ -1,5 +1,6 @@
 from textnode import TextNode
 from textnode import TextType
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -17,5 +18,8 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             new_nodes.append(node)
     return new_nodes
     
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
-
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
--- src/test_delimiter.py
+++ src/test_delimiter.py
@@ -2,12 +2,27 @@ import unittest
 from textnode import TextNode
 from textnode import TextType
 from node_delimiter import split_nodes_delimiter
+from node_delimiter import extract_markdown_images
+from node_delimiter import extract_markdown_links
 
 class TestDelimiter(unittest.TestCase):
     def test_split_nodes_delimiter_basic(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
         self.assertEqual(new_nodes, [TextNode("This is text with a ", TextType.TEXT), TextNode("code block", TextType.CODE), TextNode(" word", TextType.TEXT)])
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "Visit [Google](https://google.com) today!"
+        )
+        self.assertListEqual([("Google", "https://google.com")], matches)
+        
 
 if __name__ == "__main__":
     unittest.main()
